e0863fe8499413f37731550da83cb644313c4111
Logan Zhou <xorcererzc@gmail.com>
2013-10-09 15:06:23 +0800

    Fixed zh-cn html cv.

--- generate-resumes.sh
+++ generate-resumes.sh
@@ -14,6 +14,21 @@ pandoc -f markdown logan-zhou-resume-java.md -o logan-zhou-resume-java.html
 pandoc -f markdown logan-zhou-resume-java.md -o logan-zhou-resume-java.pdf
 pandoc -f markdown logan-zhou-resume-java.md -o logan-zhou-resume-java.docx
 
-pandoc 周翀简历.md -o 周翀简历.html --latex-engine=xelatex -V mainfont=STFangsong
+pandoc 周翀简历.md -o zh-cn-content.html --latex-engine=xelatex -V mainfont=STFangsong
+echo '
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <title>周翀简历</title>
+  </head>
+  <body>
+' > 周翀简历.html
+cat zh-cn-content.html >> 周翀简历.html
+echo '
+  </body>
+</html>
+' >> 周翀简历.html
+rm zh-cn-content.html
 pandoc 周翀简历.md -o 周翀简历.pdf --latex-engine=xelatex -V mainfont=STFangsong
 pandoc 周翀简历.md -o 周翀简历.docx --latex-engine=xelatex -V mainfont=STFangsong
